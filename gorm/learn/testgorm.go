30c02fd4b3635ca44dc6fe653b7055bd3151e84f
wangzilong <wang2001zilong@126.com>
2023-11-21 23:08:57 +0800

    11.21

---
+++ gorm/learn/testgorm.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func init() {
+	username := "root"   //账号
+	password := "123456" //密码
+	host := "127.0.0.1"  //数据库地址，可以是Ip或者域名
+	port := 3306         //数据库端口
+	Dbname := "gorm"     //数据库名
+	timeout := "10s"     //连接超时，10秒
+
+	// root:root@tcp(127.0.0.1:3306)/gorm?
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=%s", username, password, host, port, Dbname, timeout)
+	//连接MYSQL, 获得DB类型实例，用于后面的数据库读写操作。
+	db, err := gorm.Open(mysql.Open(dsn))
+	if err != nil {
+		panic("连接数据库失败, error=" + err.Error())
+	}
+	DB = db
+	// 连接成功
+	fmt.Println(db)
+}
+
+type Student struct {
+	ID   uint
+	Name string
+	Age  int
+}
+
+func main() {
+	DB.AutoMigrate(&Student{})
+}

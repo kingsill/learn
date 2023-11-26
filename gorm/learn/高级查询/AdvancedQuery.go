b127d41c5cd8a31de50198d3abdf7a842d5eb639
wangzilong <wang2001zilong@126.com>
2023-11-27 00:58:03 +0800

    11.27

--- gorm/learn/高级查询/AdvancedQuery.go
+++ gorm/learn/高级查询/AdvancedQuery.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"gorm.io/driver/mysql"
@@ -14,6 +15,15 @@ type Student struct {
 	Age    int     `gorm:"size:3"`
 	Email  *string `gorm:"size:128"`
 	Gender bool
+}
+
+type SS struct {
+	ID   uint   `gorm:"size:10"`
+	Name string `gorm:"size:16"`
+}
+
+type SSS struct {
+	Age int `gorm:"size:3"`
 }
 
 var DB *gorm.DB
@@ -40,10 +50,14 @@ func init() {
 	fmt.Println(db)
 }
 
+var SList []Student
+var SSList []SS
+var SSSList []SSS
+
 func main() {
 	//	#查询用户名是枫枫的
 	//	select * from students where name='枫枫';
-	var SList []Student
+
 	DB.Where("name=?", "枫枫").Find(&SList)
 	fmt.Println(SList)
 
@@ -77,11 +91,147 @@ func main() {
 	DB.Where("email like ? or Gender =?", "%qq%", "0").Find(&SList)
 	stringPointer(SList)
 
+	//传参的形式约束较小
 	DB.Where(" Gender =?", false).Find(&SList)
 	DB.Where(" Gender =?", "false").Find(&SList)
 	stringPointer(SList)
 	DB.Where(" Gender =?", "0").Find(&SList)
 	stringPointer(SList)
+	DB.Where(&Student{Name: "李元芳", Age: 0}).Find(&SList)
+	stringPointer(SList)
+
+	// 使用结构体查询，会过滤零值
+	DB.Where(&Student{Name: "李元芳", Age: 0}).Find(&SList)
+	// SELECT * FROM `students` WHERE `students`.`name` = '李元芳' //这里的age=0条件被过滤
+	fmt.Println(SList)
+
+	//使用map查询，不会过滤零值
+	DB.Where(map[string]any{"name": "李元芳", "age": 0}).Find(&SList)
+	// SELECT * FROM `students` WHERE `age` = 0 AND `name` = '李元芳'
+	fmt.Println(SList)
+
+	//智能选择字段 ----------------------------------------------- 使用结构体方法
+	DB.Where(&Student{Name: "李元芳", Age: 0}).Find(&SSList) //这个不行是因为这里根据find中结构体名字进行表的查询，不对应
+	// Table 'gorm.sses' doesn't exist
+
+	var NEWs SS //SS中只包含id和name
+	//查询id和name字段
+	DB.Model(&SList).Find(&SSList)   //多个获取，其后跟结构体切片
+	DB.Model(&Student{}).Find(&NEWs) //单个获取，其后跟结构体
+	//两个相同表述，sql查询语句： SELECT `students`.`id`,`students`.`name` FROM `students`
+	fmt.Println(NEWs)
+
+	//select方法
+	DB.Select("name", "age").Find(&SList)
+	fmt.Println(SList)
+	// 没有被选中，会被赋零值
+	//可以使用select+scan进行单、多列的查询
+
+	//排序 根据年龄倒序
+	DB.Order("age desc").Find(&SList)
+	fmt.Println(SList)
+	// desc    降序
+	// asc     升序
+
+	//分页查询
+	//offset = pageSize * (pageNum - 1)
+	// 一页两条，第1页
+	DB.Limit(2).Offset(0).Find(&SList)
+	fmt.Println(SList)
+	// 第2页
+	DB.Limit(2).Offset(2).Find(&SList)
+	fmt.Println(SList)
+	// 第3页
+	DB.Limit(2).Offset(4).Find(&SList)
+	fmt.Println(SList)
+
+	//去重
+	//1.通过结构体进行 单列查询 ，再进行分组
+	DB.Model(&Student{}).Distinct("age").Find(&SSSList)
+	fmt.Println(SSSList)
+	//[{32} {18} {23} {54} {14} {25} {26} {21}]
+
+	//2.通过select+scan进行 单列查询， 再分组
+	var ageList []int
+	//table 传 表名 ；model 传 表的对象
+	//DB.Table("students").Select("distinct age").Scan(&ageList) //直接在select中进行 去重
+	DB.Table("students").Select("age").Distinct("age").Scan(&ageList)
+	fmt.Println(ageList)
+	//[32 18 23 54 14 25 26 21]
+
+	//分组查询--------------------------------
+	// 查询男生的个数和女生的个数
+	DB.Table("students").Select("count(*)").Group("gender").Scan(&ageList)
+	fmt.Println(ageList)
+	//[6 3]
+
+	// 查询男生的个数和女生的个数,增添一列表明 男生、女生
+	type AggeGroup struct {
+		Gender int
+		Count  int `gorm:"column:count(id)"`
+	}
+	var agge []AggeGroup
+	DB.Table("students").Select("count(id)", "gender").Group("gender").Scan(&agge)
+	fmt.Println(agge)
+	//[{1 6} {0 3}]
+
+	//在第二个基础上，再显示男女生的名字
+	type AggeGroup2 struct {
+		Gender int
+		Count  int    `gorm:"column:count(id)"`
+		Name   string `gorm:"column:group_concat(name)"`
+	}
+	var agge2 []AggeGroup2
+	// 查询男生的个数和女生的个数
+	DB.Table("students").Select("count(id)", "gender", "group_concat(name)").Group("gender").Scan(&agge2)
+	fmt.Println(agge2)
+	//[{0 3 李琦,晓梅,如燕} {1 6 李元芳,张武,枫枫,刘大,李武,魔灵}]
+
+	//执行原生sql 以分组查询为例
+	//type AggeGroup2 struct {
+	//	Gender int
+	//	Count  int    `gorm:"column:count(id)"`
+	//	Name   string `gorm:"column:group_concat(name)"`
+	//}
+	//var agge2 []AggeGroup2
+	DB.Raw(`SELECT count(id), gender, group_concat(name) FROM students GROUP BY gender`).Scan(&agge2)
+	fmt.Println(agge2)
+	//SELECT count(id), gender, group_concat(name) FROM students GROUP BY gender
+	//[{0 3 李琦,晓梅,如燕} {1 6 李元芳,张武,枫枫,刘大,李武,魔灵}]
+
+	//子查询----------------------------查询年龄大于平均值的学生
+	var users []Student
+	DB.Model(Student{}).Where("age > (?)", DB.Model(Student{}).Select("avg(age)")).Find(&users)
+	fmt.Println(users)
+	//select * from students where age > (select avg(age) from students);
+
+	//命名参数
+	DB.Where("name = @name and age = @age", sql.Named("name", "枫枫"), sql.Named("age", 23)).Find(&SList)
+	DB.Where("name = @name and age = @age", map[string]any{"name": "枫枫", "age": 23}).Find(&SList)
+	fmt.Println(SList)
+	//SELECT * FROM `students` WHERE name = '枫枫' and age = 23
+
+	//find到map
+	var res []map[string]any
+	DB.Table("students").Find(&res)
+	for _, containt := range res {
+		fmt.Println(containt)
+	}
+
+	//查询引用scope 链式调用
+	//// 返回 age>23 的查询
+	//func Age23(db *gorm.DB) *gorm.DB {
+	//	return db.Where("age > ?", 23)
+	//}
+	DB.Scopes(Age23).Find(&SList)
+	fmt.Println(SList)
+	// SELECT * FROM `students` WHERE age > 23
+
+}
+
+// 返回 age>23 的查询
+func Age23(db *gorm.DB) *gorm.DB {
+	return db.Where("age > ?", 23)
 }
 
 // 将指针传进去，直接清空切片内容

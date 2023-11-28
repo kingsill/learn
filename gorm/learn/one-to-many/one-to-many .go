3a1f6411fe84c9118482bc6913994021eafba661
wangzilong <wang2001zilong@126.com>
2023-11-29 01:10:31 +0800

    11.29

--- gorm/learn/one-to-many/one-to-many .go
+++ gorm/learn/one-to-many/one-to-many .go
@@ -48,52 +48,86 @@ type Article struct {
 
 //重写外键关联----------------------------
 //gorm的foreignKey备注写在对应的两个表的关联上
-
-type User1 struct {
-	ID       uint       `gorm:"size:4"`
-	Name     string     `gorm:"size:8"`
-	Articles []Article1 `gorm:"foreignKey:UID"`
-}
-
-type Article1 struct {
-	ID    uint   `gorm:"size:4"`
-	Title string `gorm:"size:16"`
-	UID   uint   `gorm:"size:4"`
-	User  User1  `gorm:"foreignKey:UID"`
-}
+//
+//type User1 struct {
+//	ID       uint       `gorm:"size:4"`
+//	Name     string     `gorm:"size:8"`
+//	Articles []Article1 `gorm:"foreignKey:UID"`
+//}
+//
+//type Article1 struct {
+//	ID    uint   `gorm:"size:4"`
+//	Title string `gorm:"size:16"`
+//	UID   uint   `gorm:"size:4"`
+//	User  User1  `gorm:"foreignKey:UID"`
+//}
 
 //重写引用----------------------------
 //备注写在对应的两个表的关联上
 
-type User2 struct {
-	ID       uint       `gorm:"size:4"`
-	Name     string     `gorm:"size:8"`
-	Articles []Article2 `gorm:"foreignKey:UserName;references:Name"`
-}
-
-type Article2 struct {
-	ID       uint   `gorm:"size:4"`
-	Title    string `gorm:"size:16"`
-	UserName string `gorm:"size:8"`
-	User     User2  `gorm:"references:Name"`
-}
+//type User2 struct {
+//	ID       uint       `gorm:"size:4"`
+//	Name     string     `gorm:"size:8"`
+//	Articles []Article2 `gorm:"foreignKey:UserName;references:Name"`
+//}
+//
+//type Article2 struct {
+//	ID       uint   `gorm:"size:4"`
+//	Title    string `gorm:"size:16"`
+//	UserName string `gorm:"size:8"`
+//	User     User2  `gorm:"references:Name"`
+//}
 
 func main() {
 	//DB.AutoMigrate(&User{}, &Article{})
 	//DB.AutoMigrate(&User1{}, &Article1{})
 	//DB.AutoMigrate(&User2{}, &Article2{})
 
-	//创建用户的同时创建文章，并将两者关联
-	DB.Save(&User{
-		Name: "wang2",
-		Articles: []Article{
-			{Title: "golang"},
-			{Title: "python"},
-		},
-	})
+	////创建用户的同时创建文章，并将两者关联
+	//DB.Save(&User{
+	//	Name: "wang2",
+	//	Articles: []Article{
+	//		{Title: "golang"},
+	//		{Title: "python"},
+	//	},
+	//})
 
-	//创建文章，关联已有用户
+	////创建文章，关联已有用户
+	////1.直接传入关联外键部分
+	//DB.Save(&Article{Title: "easyGo", UserID: 2})
+	//
+	////2.查询过后传入对应结构体中
+	//var user User
+	//DB.Take(&user, 1)                           //查询已有用户
+	//DB.Save(&Article{Title: "c++", User: user}) //将关联部分的User结构体传入
+
+	//外键添加
+	//常规方法-----------------------将id为8的文章和id为2的用户绑定
+	//现有用户，这里id=2
+	//var user User
+	//DB.Take(&user, 2)
+
+	//现有文章，id=8，未设置用户
+	//var article Article
+	//DB.Take(&article, 8)
+
+	//方法1.给现有用户绑定文章
+	//user.Articles = []Article{article}//[{8 c语言 2 {0  []}}]
+	//DB.Save(&user)
+
+	//方法2.给现有文章关联用户
+	//article.User = user //{2 wang2 []}
+	//DB.Save(&article)
+	//-------------------------------------
+	//append方法--------将id为8的文章和id为2的用户绑定
 	var user User
-	DB.Take(&user, 1)                           //查询已有用户
-	DB.Save(&Article{Title: "c++", User: user}) //将关联部分的User结构体传入
+	DB.Take(&user, 2)
+	var article Article
+	DB.Take(&article, 8)
+	//1.用户绑定文章
+	//model在选表的同时也在选择对象
+	//DB.Model(&User{ID: 2}).Association("Articles").Append(&article)
+	DB.Model(&user).Association("Articles").Append(&article)
+	//2.文章关联用户
+	DB.Model(&article).Association("User").Append(&user)
 }
